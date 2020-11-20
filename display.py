bf7d8218d7c99fe3c4df2e53a7b016da2f4d1a70
Raffael Georg Krakau <raffael-georg.krakau@informatik.hs-fulda.de>
2020-11-20 23:22:09 +0100

    clean Display class

--- display.py
+++ display.py
@@ -1,39 +1,27 @@
-import pygame as pg
-
-
+from ASCIIRender import ASCIIRender
 class Display:
     def __init__(self):
         from tetris import config
         self.config = config
-        # game ground
-        self.config.pictures = []
-        for n in range(1, 8):
-            try:
-                self.config.pictures.append(
-                    pg.transform.scale(pg.image.load("res/tetrisblock_%d.gif" % n), (self.config.blocksize, self.config.blocksize)))
-            except FileNotFoundError as e:
-                print(e.errno)
+        if config.is_console_mode:
+            self.ASCIIRender = ASCIIRender()
 
-        # show screen
-        pg.init()
-        self.screen = pg.display.set_mode([self.config.width, self.config.height])
-        self.screen.fill((0, 0, 0))
+
+            #show bash
+        else:
+            pass
+            #pygameGui
+
+        # game ground
 
     def run(self):
-        for n, color in enumerate(self.config.grid):  # from 0 to 199
-            if color > 0:
-                x = n % self.config.columns * self.config.distance
-                y = n // self.config.columns * self.config.distance
-                self.screen.blit(self.config.pictures[color], (x, y))
-        pg.display.flip()
+        self.ASCIIRender.run()
 
     def getEvents(self):
-        return pg.event.get()
+        pass
 
     def getPgQuitEvent(self):
-        return pg.QUIT
+        pass
 
     def quit(self):
-        pg.quit()
-        # todo: find a way to quit in a proper way
-        exit(0)
+        pass
